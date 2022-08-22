cc772a6f0db45f828bdeac2e907780b4c20e06d0
ylzblc <109525587+ylzblc@users.noreply.github.com>
2022-08-22 23:08:38 +0300

    terssayi-recursive-
    Kullanıcıdan aldığı sayının tersini veren recursive kod.

---
+++ recursiveterssayi/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int ters(int sayi, int len)
+{
+    if (len == 1)
+    {
+        return sayi;
+    }
+    else
+    {
+        return (((sayi % 10) * pow(10, len - 1)) + ters(sayi / 10, --len));
+    }
+}
+
+int main()
+{
+    int sayi, sonuc;
+    int uzunluk = 0, temp;
+
+    printf("Bir sayi giriniz: ");
+    scanf("%d", &sayi);
+    temp = sayi;
+    while (temp != 0)
+    {
+        uzunluk++;
+        temp = temp / 10;
+    }
+    sonuc = ters(sayi, uzunluk);
+    printf("%d sayisinin tersi %d.\n", sayi, sonuc);
+    return 0;
+}
